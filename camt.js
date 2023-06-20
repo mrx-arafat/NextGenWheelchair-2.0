2e6bcba3b475b560221d2cea84441514f6c7a45a
mrx-arafat <arafatmrx@gmail.com>
2023-06-20 17:17:29 +0600

    login page handle

--- camt.js
+++ camt.js
@@ -1,18 +1,69 @@
-// Get the video element
+// Get the video, canvas, and button elements
 const videoElement = document.getElementById("videoElement");
+const canvas = document.createElement("canvas");
+const button = document.getElementById("webcamButton");
 
-// Check if the browser supports the necessary APIs
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Access the camera
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      // Set the camera stream as the source of the video element
-      videoElement.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error("Error accessing camera:", error);
-    });
-} else {
-  console.error("Camera access not supported by the browser.");
+// Set up the socket.io client
+const socket = io();
+
+// Keep track of whether the webcam is currently on or off
+let webcamOn = false;
+
+// Keep track of the interval that sends frames to the server
+let frameInterval = null;
+
+button.addEventListener("click", () => {
+  if (webcamOn) {
+    // If the webcam is on, stop the stream
+    videoElement.srcObject.getTracks().forEach((track) => track.stop());
+    clearInterval(frameInterval);
+    button.textContent = "Start Webcam";
+  } else {
+    // If the webcam is off, start the stream
+    startWebcam();
+    button.textContent = "Stop Webcam";
+  }
+
+  webcamOn = !webcamOn;
+});
+
+// Function to start the webcam and send frames to the server
+function startWebcam() {
+  // Check if the browser supports the necessary APIs
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Access the camera
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 640, height: 480 } }) // Specify resolution
+      .then((stream) => {
+        // Set the camera stream as the source of the video element
+        videoElement.srcObject = stream;
+
+        // Play the videoElement to start showing the stream
+        videoElement.play();
+
+        // Set the videoElement's height and width to match the stream
+        videoElement.width = stream.getTracks()[0].getSettings().width;
+        videoElement.height = stream.getTracks()[0].getSettings().height;
+
+        // Start a loop that sends frames to the server
+        frameInterval = setInterval(() => {
+          canvas
+            .getContext("2d")
+            .drawImage(
+              videoElement,
+              0,
+              0,
+              videoElement.width,
+              videoElement.height
+            );
+          const dataUrl = canvas.toDataURL("image/jpeg", 0.8);
+          socket.emit("stream", dataUrl);
+        }, 1000 / 30); // Send 30 frames per second
+      })
+      .catch((error) => {
+        console.error("Error accessing camera:", error);
+      });
+  } else {
+    console.error("Camera access not supported by the browser.");
+  }
 }
--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const socketIo = require("socket.io");
 
-const serverIP = "192.168.0.102"; // Replace with your server's IP address
+const serverIP = "10.103.129.60"; // Replace with your server's IP address
 
 app.use(express.static(path.join(__dirname, "/")));
-app.get("/control-head_test.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "control-head_test.html"));
+app.get("/camt.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "camt.html"));
 });
 
 const port = 3000; // Choose any available port number
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("server.key"), // path to your key file
-      cert: fs.readFileSync("server.cert"), // path to your cert file
-    },
-    app
-  )
-  .listen(port, serverIP, () => {
-    console.log(`Web server running at https://${serverIP}:${port}`);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("server.key"), // path to your key file
+    cert: fs.readFileSync("server.cert"), // path to your cert file
+  },
+  app
+);
+
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // When a message with the 'stream' event is received, broadcast it to all other clients
+  socket.on("stream", (image) => {
+    socket.broadcast.emit("stream", image);
   });
+});
+
+server.listen(port, serverIP, () => {
+  console.log(`Web server running at https://${serverIP}:${port}`);
+});
